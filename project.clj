56c3de0c4276eac28f56efffb23b28582a7efc76
Thomas G. Kristensen <tgk@trifork.com>
2013-01-26 19:16:44 +0100

    Add description in project.clj and upgrade cljsbuild configuration.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject edn-service-stub "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "A spike demonstrating how edn can be used in a REST service"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
@@ -8,7 +8,9 @@
                  [compojure "1.1.3"]]
   :plugins [[lein-ring "0.7.5"]
             [lein-cljsbuild "0.3.0"]]
-  :cljsbuild {:builds [{:source-path "src-cljs"
-                        :compiler {:optimizations :advanced
-                                   :output-to "resources/public/js/compiled.js"}}]}
+  :cljsbuild {:builds
+              [{:source-paths ["src-cljs"],
+                :compiler
+                {:output-to "resources/public/js/compiled.js",
+                 :optimizations :advanced}}]}
   :ring {:handler edn-service-stub.routes/handler})
