c948effedc95d973803d565e96614f4d3907574b
Mihai Giurgeanu <mihai.giurgeanu@genesissoftware.ro>
2013-12-18 08:23:39 +0200

    Fixed lein-ring plugin incompatibility.

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [ring "1.1.6"]
+                 [ring "1.2.1"]
                  [ring-edn "0.1.0"]
-                 [compojure "1.1.3"]]
-  :plugins [[lein-ring "0.7.5"]
+                 [compojure "1.1.6"]]
+  :plugins [[lein-ring "0.8.8"]
             [lein-cljsbuild "0.3.0"]]
   :cljsbuild {:builds
               [{:source-paths ["src-cljs"],
